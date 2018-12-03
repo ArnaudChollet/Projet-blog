57583d8bab5bccee2a5419c0cbfc1a90dd15226c
achollet <arnaud.chollet@gmail.com>
2018-12-03 11:10:47 +0100

    Initialisation projet BLOG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'projet-blog';
+  title = 'MY BLOG';
+
+  PostListItemComponent = [
+    {
+      title: 'Mon premier post',
+      content: 'Lorem Ipsum',
+      loveIts: 0,
+      created_at: new Date()
+    },
+    {
+      title: 'Mon deuxième post',
+      content: 'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum',
+      loveIts: 0,
+      created_at: new Date()
+    },
+    {
+      title: 'Encore un post',
+      content: 'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum',
+      loveIts: 0,
+      created_at: new Date()
+    }
+  ];
 }
+
